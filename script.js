531ebb70d5708cb50fcf18e69e860ddbe1842ae5
LeilaSchooley <vortex962@zoho.com>
2021-08-04 17:23:40 +0100

    Added arguments for calculator functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(number, numberOne){
+
+}
+function subtract(number, numberOne){
+
+}
+function multiply(number, numberOne){
+
+}
+function divide(number, numberOne){
+
+}
+function operate(number, numberOne){
+    return multiply(number, numberOne)
+
+}
